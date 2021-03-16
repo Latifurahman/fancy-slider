59a51e071492e72addea19ed33c6c8350fed6825
Latifur Rahman <latifurrahman350@gmail.com>
2021-03-17 02:31:07 +0600

    project done

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ const showImages = (images) => {
   images.forEach(image => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">
+    <p class="likes">Likes:${image.likes}</p>
+    <p class="comments">Comments:${image.comments}</p>
+    `;
+
     gallery.appendChild(div);
     
   });
